9e111d04b50616e686118629da0a6faf118ad7ca
김지후 <rlaqltmxm@naver.com>
2019-10-29 21:46:12 +0900

    Updates

---
+++ precache-manifest.da17413b63b313af165995794d930706.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88d664ee66443bdeb4995da21665f104",
+    "url": "/graduation-project-front/index.html"
+  },
+  {
+    "revision": "029f1f88639b00440e17",
+    "url": "/graduation-project-front/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "f93ddf5b6ca41c2c77e2",
+    "url": "/graduation-project-front/static/css/main.b1f82f48.chunk.css"
+  },
+  {
+    "revision": "029f1f88639b00440e17",
+    "url": "/graduation-project-front/static/js/2.cb993752.chunk.js"
+  },
+  {
+    "revision": "f93ddf5b6ca41c2c77e2",
+    "url": "/graduation-project-front/static/js/main.6c04e873.chunk.js"
+  },
+  {
+    "revision": "12b3927f7b2deda6828c",
+    "url": "/graduation-project-front/static/js/runtime~main.f1f03341.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ec5c53e49b40fa8d2e28c6c36ab6db6e.js"
+  "/graduation-project-front/precache-manifest.da17413b63b313af165995794d930706.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/graduation-project-front/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
